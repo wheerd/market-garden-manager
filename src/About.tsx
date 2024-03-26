0d73a75b4eee17bc94be834c40588231f047e8fc
Manuel Krebber <admin@wheerd.de>
2024-03-26 21:23:26 +0100

    Add basic routing

---
+++ src/About.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const About: React.FC = () =>
+    <div>
+        <p>
+            About page.
+        </p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn React
+        </a>
+    </div>
+
+export default About
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import About from './About';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload (test edit).
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
