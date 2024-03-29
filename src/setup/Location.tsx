2ec5c37e81cf41c6d9b04a3df5f543ebb5b6e3e3
Manuel Krebber <admin@wheerd.de>
2024-03-29 22:12:02 +0100

    Persist location

--- src/setup/Location.tsx
+++ src/setup/Location.tsx
@@ -1,17 +1,34 @@
-import React from "react"
+import React, { useEffect, useMemo } from "react"
 import Map, { MapboxEvent } from "react-map-gl"
 import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+import { usePersistedState } from "../lib/usePersistedState";
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
+interface LocationData {
+    longitude: number,
+    latitude: number,
+    zoom: number
+}
+
 const Location: React.FC = () => {
-    const [viewState, setViewState] = React.useState({
+    const defaultLocation = useMemo(() => ({
         longitude: -100,
         latitude: 40,
         zoom: 3.5
-    });
+    }), [])
+
+    const [location, setLocation] = usePersistedState<LocationData>("location", defaultLocation);
+
+    const [viewState, setViewState] = React.useState(defaultLocation);
+
+    useEffect(() => {
+        if (typeof location !== "undefined") {
+            setViewState(location)
+        }
+    }, [location])
 
     function onLoad(e: MapboxEvent) {
         var geocoder = new MapboxGeocoder({
@@ -24,7 +41,18 @@ const Location: React.FC = () => {
     return (
         <div>
             Location
-            <p>Lat: {viewState.latitude} | Lon: {viewState.longitude} | Zoom: {viewState.zoom}</p>
+            <p>
+                Lat:
+                <input type="text" value={viewState.latitude} onChange={(event) => {
+                    setViewState({ ...viewState, latitude: +event.target.value })
+                }}></input>
+                | Lon:
+                <input type="text" value={viewState.longitude} onChange={(event) => {
+                    setViewState({ ...viewState, longitude: +event.target.value })
+                }}></input>
+                | Zoom: {viewState.zoom}
+                <button onClick={() => setLocation(viewState)}>Save Location</button>
+            </p>
             <Map
                 onLoad={onLoad}
                 mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
