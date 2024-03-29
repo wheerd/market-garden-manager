5fdaa1b290328e52276c45ef6e3db47ee6297338
Manuel Krebber <admin@wheerd.de>
2024-03-29 21:04:21 +0100

    Add geocoder to map

--- src/setup/Location.tsx
+++ src/setup/Location.tsx
@@ -1,7 +1,10 @@
 import React from "react"
-import Map from "react-map-gl"
+import Map, { MapboxEvent } from "react-map-gl"
+import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 const Location: React.FC = () => {
     const [viewState, setViewState] = React.useState({
@@ -10,11 +13,20 @@ const Location: React.FC = () => {
         zoom: 3.5
     });
 
+    function onLoad(e: MapboxEvent) {
+        var geocoder = new MapboxGeocoder({
+            accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN || "",
+            mapboxgl: mapboxgl
+        });
+        e.target.addControl(geocoder);
+    }
+
     return (
         <div>
             Location
             <p>Lat: {viewState.latitude} | Lon: {viewState.longitude} | Zoom: {viewState.zoom}</p>
             <Map
+                onLoad={onLoad}
                 mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
                 reuseMaps
                 {...viewState}
