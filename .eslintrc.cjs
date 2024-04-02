43bded2347fa57ae12b60d171a96229076309906
Manuel Krebber <admin@wheerd.de>
2024-04-02 12:26:46 +0200

    Fix prettier config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,26 +2,12 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked',
+    "./node_modules/gts/",
     'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'react'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/restrict-template-expressions': [
-      'error',
-      { allowNumber: true },
-    ],
-  },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
