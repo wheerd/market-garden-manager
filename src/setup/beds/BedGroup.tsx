f5fd4800e8e9f19cbd6e43fac5a05292c41630ed
Manuel Krebber <admin@wheerd.de>
2024-04-06 16:21:08 +0200

    Refactor

---
+++ src/setup/beds/BedGroup.tsx
@@ -0,0 +1,131 @@
+import React, {PointerEvent, useMemo, useState} from 'react';
+
+import {
+  constrainToBox,
+  getOffsetBBox,
+  transformRect,
+} from '@/lib/domGeometryUtils';
+
+import './BedGroup.scss';
+
+function getMousePosition(evt: PointerEvent<SVGElement>) {
+  const CTM = evt.currentTarget.ownerSVGElement!.getScreenCTM()!;
+  const point = new DOMPoint(evt.clientX, evt.clientY);
+  return point.matrixTransform(CTM.inverse());
+}
+
+export interface BedGroupOptions {
+  x: number;
+  y: number;
+  length: number;
+  width: number;
+  count: number;
+  spacing: number;
+  active: boolean;
+}
+
+export const BedGroup: React.FC<BedGroupOptions> = ({
+  x,
+  y,
+  length,
+  width,
+  count,
+  spacing,
+  active,
+}) => {
+  const [position, setPosition] = useState({x, y});
+  const [dragging, setDragging] = useState(false);
+  const [dragStartMousePosition, setDragStartMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [dragOffset, setDragOffset] = useState({x: 0, y: 0});
+  const [dragOffsetBBox, setDragOffsetBBox] = useState(new DOMRect(0, 0, 0, 0));
+
+  const bedXs = useMemo(
+    () => [...Array(count).keys()].map(i => spacing + (width + spacing) * i),
+    [count, width, spacing]
+  );
+  const totalWidth = useMemo(
+    () => count * (width + spacing) + spacing,
+    [count, width, spacing]
+  );
+  const totalHeight = useMemo(() => length + 2 * spacing, [length, spacing]);
+
+  function onDragStart(evt: PointerEvent<SVGGraphicsElement>) {
+    const element = evt.currentTarget;
+    element.setPointerCapture(evt.pointerId);
+    const mousePosition = getMousePosition(evt);
+    setDragging(true);
+    setDragOffset({x: 0, y: 0});
+    setDragStartMousePosition({x: mousePosition.x, y: mousePosition.y});
+    initOffsetBBox(element);
+  }
+
+  function initOffsetBBox(element: SVGGraphicsElement) {
+    const svg = element.ownerSVGElement!;
+    const clientBBox = element.getBoundingClientRect();
+    const svgBBox = transformRect(clientBBox, svg.getScreenCTM()!.inverse());
+    const viewBox = DOMRect.fromRect(svg.viewBox.baseVal!);
+    setDragOffsetBBox(getOffsetBBox(svgBBox, viewBox));
+  }
+
+  function onDrag(evt: PointerEvent<SVGGraphicsElement>) {
+    if (dragging) {
+      evt.preventDefault();
+      const mousePosition = getMousePosition(evt);
+      const newOffset = new DOMPoint(
+        mousePosition.x - dragStartMousePosition.x,
+        mousePosition.y - dragStartMousePosition.y
+      );
+      const constrainedOffset = constrainToBox(newOffset, dragOffsetBBox);
+      setDragOffset({
+        x: constrainedOffset.x,
+        y: constrainedOffset.y,
+      });
+    }
+  }
+
+  function onDragEnd(evt: PointerEvent<SVGGraphicsElement>) {
+    evt.currentTarget.releasePointerCapture(evt.pointerId);
+    if (dragging) {
+      setPosition({
+        x: dragOffset.x + position.x,
+        y: dragOffset.y + position.y,
+      });
+      setDragOffset({x: 0, y: 0});
+      setDragging(false);
+    }
+  }
+
+  return (
+    <g
+      transform={
+        `translate(${dragOffset.x}, ${dragOffset.y}) ` +
+        `translate(${position.x}, ${position.y})`
+      }
+      onPointerMove={onDrag}
+      onPointerDown={onDragStart}
+      onPointerUp={onDragEnd}
+      className={`bed-group-ctrl ${active ? 'active' : ''}`}
+    >
+      <rect
+        x={0}
+        y={0}
+        width={totalWidth}
+        height={totalHeight}
+        className="group-outline"
+      ></rect>
+      {bedXs.map((xOff, i) => (
+        <rect
+          key={`bed${i}`}
+          x={xOff}
+          y={spacing}
+          width={width}
+          height={length}
+          className="bed"
+        ></rect>
+      ))}
+    </g>
+  );
+};
--- src/setup/beds/BedOverlay.tsx
+++ src/setup/beds/BedOverlay.tsx
@@ -1,162 +1,17 @@
-import React, {PointerEvent, useMemo, useState} from 'react';
+import React from 'react';
 
-import {BedGroup} from '@/model/beds';
+import {BedGroup as BedGroupModel} from '@/model/beds';
+
+import {BedGroup} from './BedGroup';
 
 import './BedOverlay.scss';
 
-interface BedGroupOptions {
-  x: number;
-  y: number;
-  length: number;
-  width: number;
-  count: number;
-  spacing: number;
-  active: boolean;
-}
-
-function transformRect(rect: DOMRect, svg: SVGSVGElement): DOMRect {
-  const inverseCTM = svg.getScreenCTM()!.inverse();
-  const topLeft = new DOMPoint(rect.left, rect.top).matrixTransform(inverseCTM);
-  const bottomRight = new DOMPoint(rect.right, rect.bottom).matrixTransform(
-    inverseCTM
-  );
-  return new DOMRect(
-    topLeft.x,
-    topLeft.y,
-    bottomRight.x - topLeft.x,
-    bottomRight.y - topLeft.y
-  );
-}
-
-function getMousePosition(evt: PointerEvent<SVGGraphicsElement>) {
-  const CTM = evt.currentTarget.ownerSVGElement!.getScreenCTM()!;
-  const point = new DOMPoint(evt.clientX, evt.clientY);
-  return point.matrixTransform(CTM.inverse());
-}
-
-function constrainToBox(point: DOMPoint, box: DOMRect): DOMPoint {
-  const constrainedX = Math.max(Math.min(point.x, box.right), box.left);
-  const constrainedY = Math.max(Math.min(point.y, box.bottom), box.top);
-  return new DOMPoint(constrainedX, constrainedY);
-}
-
-function getOffsetBBox(inner: DOMRect, outer: DOMRect): DOMRect {
-  const offsetMinX = outer.left - inner.left;
-  const offsetMaxX = outer.right - inner.right;
-  const offsetMinY = outer.top - inner.top;
-  const offsetMaxY = outer.bottom - inner.bottom;
-  return new DOMRect(
-    offsetMinX,
-    offsetMinY,
-    offsetMaxX - offsetMinX,
-    offsetMaxY - offsetMinY
-  );
-}
-
-const BedGroup: React.FC<BedGroupOptions> = ({
-  x,
-  y,
-  length,
-  width,
-  count,
-  spacing,
-  active,
-}) => {
-  const [dragging, setDragging] = useState(false);
-  const [coordinates, setCoordinates] = useState({x, y});
-  const [initialMouse, setInitialMouse] = useState({x: 0, y: 0});
-  const [offset, setOffset] = useState({x: 0, y: 0});
-  const [offsetBBox, setOffsetBBox] = useState<DOMRect>(
-    new DOMRect(0, 0, 0, 0)
-  );
-
-  const bedXs = useMemo(
-    () => [...Array(count).keys()].map(i => spacing + (width + spacing) * i),
-    [count, width, spacing]
-  );
-  const totalWidth = useMemo(
-    () => count * (width + spacing) + spacing,
-    [count, width, spacing]
-  );
-  const totalHeight = useMemo(() => length + 2 * spacing, [length, spacing]);
-
-  function onDragStart(evt: PointerEvent<SVGGraphicsElement>) {
-    setDragging(true);
-    const element = evt.currentTarget,
-      svg = element.ownerSVGElement!;
-    element.setPointerCapture(evt.pointerId);
-    const mousePosition = getMousePosition(evt);
-    setInitialMouse({x: mousePosition.x, y: mousePosition.y});
-    setOffset({x: 0, y: 0});
-
-    const clientBBox = element.getBoundingClientRect();
-    const svgBBox = transformRect(clientBBox, svg);
-    const viewBox = DOMRect.fromRect(svg.viewBox.baseVal!);
-    setOffsetBBox(getOffsetBBox(svgBBox, viewBox));
-  }
-
-  function onDrag(evt: PointerEvent<SVGGraphicsElement>) {
-    if (dragging) {
-      evt.preventDefault();
-      const mousePosition = getMousePosition(evt);
-      const newOffset = new DOMPoint(
-        mousePosition.x - initialMouse.x,
-        mousePosition.y - initialMouse.y
-      );
-      const constrainedOffset = constrainToBox(newOffset, offsetBBox);
-      setOffset({
-        x: constrainedOffset.x,
-        y: constrainedOffset.y,
-      });
-    }
-  }
-
-  function onDragEnd(evt: PointerEvent<SVGGraphicsElement>) {
-    evt.currentTarget.releasePointerCapture(evt.pointerId);
-    if (dragging) {
-      setCoordinates({
-        x: offset.x + coordinates.x,
-        y: offset.y + coordinates.y,
-      });
-      setOffset({x: 0, y: 0});
-      setDragging(false);
-    }
-  }
-
-  return (
-    <g
-      transform={`translate(${offset.x}, ${offset.y}) translate(${coordinates.x}, ${coordinates.y})`}
-      onPointerMove={onDrag}
-      onPointerDown={onDragStart}
-      onPointerUp={onDragEnd}
-      className={active ? 'active' : ''}
-    >
-      <rect
-        x={0}
-        y={0}
-        width={totalWidth}
-        height={totalHeight}
-        className="group-outline"
-      ></rect>
-      {bedXs.map((xOff, i) => (
-        <rect
-          key={`bed${i}`}
-          x={xOff}
-          y={spacing}
-          width={width}
-          height={length}
-          className="bed"
-        ></rect>
-      ))}
-    </g>
-  );
-};
-
-interface BedOverlayOptions {
+export interface BedOverlayOptions {
   sizeInMeters: number;
-  bedGroups: BedGroup[];
+  bedGroups: BedGroupModel[];
   selectedBedId: string;
 }
+
 export const BedOverlay: React.FC<BedOverlayOptions> = ({
   sizeInMeters,
   bedGroups,
